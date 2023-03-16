e30267fb0861d093b5dd27e04249680acba70a2d
Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2023-03-15 22:15:25 -0400

    Add favicon and reorganize some config options.

--- docs/conf.py
+++ docs/conf.py
@@ -22,12 +22,14 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "furo"
-html_static_path = ["_static"]
+html_favicon = "_static/favicon.ico"
+html_title = "YScope Docs"
 html_show_copyright = False
+html_static_path = ["_static"]
+
+html_theme = "furo"
 html_theme_options = {
     "source_repository": "https://github.com/y-scope/yscope-docs/",
     "source_branch": "main",
     "source_directory": "docs",
 }
-html_title = "YScope Docs"
