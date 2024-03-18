fa3acd023868d9888927df61342efe6eee5929d3
kirkrodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2024-03-18 11:35:15 -0400

    Apply suggestions from code review
    Co-authored-by: Junhao Liao <junhao@junhao.ca>

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@
 
 project = "YScope"
 # NOTE: We don't include a period after "Inc" since the theme adds one already.
-copyright = "2023-2024 YScope Inc"
+project_copyright = "2023-2024 YScope Inc"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -61,5 +61,6 @@ html_context = {
 # -- Theme custom CSS and JS ---------------------------------------------------
 # https://pydata-sphinx-theme.readthedocs.io/en/stable/user_guide/static_assets.html
 
+
 def setup(app):
-  app.add_css_file("custom.css")
+    app.add_css_file("custom.css")
