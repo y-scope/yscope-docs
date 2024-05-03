e1f81988ef8388709b31ab4da4d0fdfd01fc4b2c
kirkrodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2024-05-03 15:00:41 -0400

    Add config file to support docs version switcher for clp. (#10)

--- server/src/main.js
+++ server/src/main.js
@@ -1,10 +1,11 @@
-import routes from "./routes.js";
-
-import "dotenv/config.js";
 import Fastify from "fastify";
 import path from "node:path";
 import process from "node:process";
 
+import fastifyCors from "@fastify/cors";
+import "dotenv/config.js";
+
+import routes from "./routes.js";
 
 // eslint-disable-next-line new-cap
 const fastify = Fastify({
@@ -13,6 +14,12 @@ const fastify = Fastify({
 
 const start = async () => {
     try {
+        // Allow CORs for http://localhost:<port>
+        await fastify.register(fastifyCors, {
+            origin: /^http:\/\/localhost:\d+$/,
+            methods: ["GET"],
+        });
+
         await fastify.register(routes, {
             publicDir: path.resolve(process.env.PUBLIC_DIR),
             projectsConfigFile: path.resolve(process.env.PROJECTS_CONFIG_FILE),
